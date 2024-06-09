2a114b103bec6d5727c005aba8798ef38bdffcb5
JTrenerry <jacksontrenerry01@gmail.com>
2024-06-09 17:41:25 +1000

    auto update at 09-06-24-17-41

--- setup.sh
+++ setup.sh
@@ -3,20 +3,18 @@
 
 # Move scripts to /usr/local/bin and set permissions
 chmod +x ./shell/*
-cp -r ./shell/* /usr/local/bin/
+sudo cp -r ./shell/* /usr/local/bin/
 
 # Move services to /etc/systemd/system
-for file in ./services; do
-    cp $file "/etc/systemd/system/$file.service";
-done
+sudo cp -r ./services/* /etc/systemd/system/
 
 # Reload systemd
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 # Move configs
 cp ./conf/.bashrc ~/.bashrc
 cp ./conf/.vimrc ~/.vimrc
-cp ./conf/.tmux.config ~/.config/tmux/tmux.config
+cp ./conf/tmux.config ~/.config/tmux/tmux.config
 cp ./conf/.gitconfig ~/.gitconfig
 cp ./conf/init.vim ~/.config/nvim/init.vim
 
