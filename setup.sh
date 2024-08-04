3a0119a2b395a8f473e523a0941e97cda8e6a484
JTrenerry <jacksontrenerry01@gmail.com>
2024-08-04 14:36:29 +1000

    auto update at 04-08-24-14-36

--- setup.sh
+++ setup.sh
@@ -16,8 +16,9 @@ cp ./conf/.bashrc ~/.bashrc
 cp ./conf/.vimrc ~/.vimrc
 
 cp ./conf/tmux.config ~/.config/tmux/tmux.config
+cp ./conf/tmux.reset.config ~/.config/tmux/tmux.reset.conf
 rm -f ~/.tmux.conf
-ln -s ~/.config/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.config/tmux/tmux.config ~/.tmux.conf
 
 cp ./conf/.gitconfig ~/.gitconfig
 cp ./conf/init.vim ~/.config/nvim/init.vim
