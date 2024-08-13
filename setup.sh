14b42cb254f8e29df884ada823590be5bf225580
JTrenerry <jacksontrenerry01@gmail.com>
2024-08-13 20:16:36 +1000

    auto update at 13-08-24-20-16

--- setup.sh
+++ setup.sh
@@ -7,37 +7,19 @@
 
 # Move scripts to /usr/local/bin and set permissions
 chmod +x ./shell/*
-sudo cp -r ./shell/* /usr/local/bin/
+sudo ln -f ./shell/* /usr/local/bin/
 
 # Move services to /etc/systemd/system
-sudo cp -r ./services/backup.service /etc/systemd/system/
+sudo ln -f ./services/backup.service /etc/systemd/system/
 sudo systemctl enable backup.service
 
 # Reload systemd
 sudo systemctl daemon-reload
 
 # Move configs
-cp ./conf/.bashrc ~/.bashrc
-cp ./conf/.vimrc ~/.vimrc
-cp ./conf/.gitconfig ~/.gitconfig
-cp ./conf/init.vim ~/.config/nvim/init.vim
+ln -f ./conf/.bashrc ~/.bashrc
+ln -f ./conf/.vimrc ~/.vimrc
+ln -f ./conf/.gitconfig ~/.gitconfig
 
 # Tmux
-cp ./conf/tmux.config ~/.config/tmux/tmux.config
-cp ./conf/tmux.reset.config ~/.config/tmux/tmux.reset.conf
-rm -f ~/.tmux.conf
-ln -s ~/.config/tmux/tmux.config ~/.tmux.conf
-
-# Package managers
-# Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Install tmux plugin manager
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
-
-# Install neovim config
-git clone https://github.com/JTrenerry/Neovim-Config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+ln -f ./conf/tmux.config ~/.tmux.conf
